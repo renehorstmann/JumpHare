4e389fc816e2e04ad7572529caee5a95c6d5f724
renehorstmann <r.horsty@gmail.com>
2021-02-15 16:04:00 +0100

    mathc update

--- src/hare.c
+++ src/hare.c
@@ -1,3 +1,4 @@
+#include "mathc/float.h"
 #include "r/ro_single.h"
 #include "r/texture.h"
 #include "u/pose.h"
@@ -6,66 +7,62 @@
 
 
 static struct {
-	rRoSingle ro;
-	float dx;
-	float looking_left;
-	
-	rRoSingle bg;
+    rRoSingle ro;
+    float dx;
+    float looking_left;
+
+    rRoSingle bg;
 } L;
 
 void hare_init() {
-	r_ro_single_init(&L.ro, camera.gl, r_texture_init_file("res/hare.png", NULL));
-	
-	r_ro_single_init(&L.bg, camera.gl, r_texture_init_file("res/grass.png", NULL));
+    r_ro_single_init(&L.ro, camera.gl, r_texture_init_file("res/hare.png", NULL));
+
+    r_ro_single_init(&L.bg, camera.gl, r_texture_init_file("res/grass.png", NULL));
 }
 
 void hare_update(float dtime) {
-	static float x=0;
-	x+=dtime * L.dx;
-	if(x>camera_right())
-	    x = camera_left();
-	if(x<camera_left())
-	    x = camera_right();
-	float px = floorf(x);
-	
-	
-	float fps = 6;
-	int frames = 4;
-	static float time=0;
-	time = fmodf(time + dtime, frames/fps);
-	int frame = time*fps;
-	
-	float w = 1.0/4.0;
-	float h = 1.0/2.0;
-	
-    if(L.dx<0)
+    static float x = 0;
+    x += dtime * L.dx;
+    if (x > camera_right())
+        x = camera_left();
+    if (x < camera_left())
+        x = camera_right();
+    float px = floorf(x);
+
+
+    float fps = 6;
+    int frames = 4;
+    static float time = 0;
+    time = fmodf(time + dtime, frames / fps);
+    int frame = time * fps;
+
+    float w = 1.0 / 4.0;
+    float h = 1.0 / 2.0;
+
+    if (L.dx < 0)
         L.looking_left = true;
-    if(L.dx>0)
+    if (L.dx > 0)
         L.looking_left = false;
-    
-    if(L.looking_left)
+
+    if (L.looking_left)
         w = -w;
-	
-	float v = L.dx==0? 0 : 1;
-	
-	u_pose_set(&L.ro.rect.pose, px, 0, 32, 32, 0);
-	u_pose_set(&L.ro.rect.uv, frame*w, v*h, w, h, 0);
-	
-	u_pose_set(&L.bg.rect.pose, 0, -5, 128*2, 64, 0);
-	u_pose_set_w(&L.bg.rect.uv, 2);
+
+    float v = L.dx == 0 ? 0 : 1;
+
+    u_pose_set(&L.ro.rect.pose, px, 0, 32, 32, 0);
+    u_pose_set(&L.ro.rect.uv, frame * w, v * h, w, h, 0);
+
+    u_pose_set(&L.bg.rect.pose, 0, -5, 128 * 2, 64, 0);
+    u_pose_set_w(&L.bg.rect.uv, 2);
 }
 
 void hare_render() {
-	r_ro_single_render(&L.bg);
-	r_ro_single_render(&L.ro);
+    r_ro_single_render(&L.bg);
+    r_ro_single_render(&L.ro);
 }
 
 
 void hare_set_speed(float dx) {
-	if(fabs(dx) < 10)
-        dx = 0;
-    if(fabs(dx) > 80) 
-        dx = 100 * dx/fabs(dx);
-	L.dx = dx;
+    L.dx = sca_sign(dx) * sca_clamp(sca_abs(dx), 10, 80);
 }
 
