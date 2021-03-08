4843711ec6bb5110c37a30757127a63008e24b22
renehorstmann <r.horsty@gmail.com>
2021-03-08 16:20:52 +0100

    gpush

--- include/hare.h
+++ include/hare.h
@@ -3,11 +3,21 @@
 
 #include "mathc/types/float.h"
 
+enum hare_state {
+	HARE_GROUNDED,
+	HARE_FALLING,
+	HARE_JUMPING,
+	HARE_DOUBLE_JUMP,
+	HARE_NUM_STATES
+};
+
 void hare_init();
 
 void hare_update(float dtime);
 
 void hare_render();
+
+enum hare_state hare_state();
 
 vec2 hare_position();
 
--- src/hare.c
+++ src/hare.c
@@ -11,14 +11,20 @@
 #define MIN_SPEED_X 20
 #define MAX_SPEED_X 100
 #define MAX_SPEED_Y 200
+#define JUMP_SPEED 200
+#define DOUBLE_JUMP_SPEED 200
 #define ACC 160
 #define DEACC 240
 #define GRAVITY -400
+
+#define SET_JUMP_TIME -0.05
 
 static struct {
 	rRoSingle ro;
 	rRoSingle coll[8];
 	rRoText input_text;
+	
+	enum hare_state state;
 	
 	vec2 pos;
 	vec2 last_pos;
@@ -29,8 +35,7 @@ static struct {
 	float jump_time;
 	
 	float set_speed_x;
-	bool set_jump;
-	bool grounded;
+	float set_jump_time;
 } L;
 
 
@@ -57,8 +62,38 @@ static void apply_speed_y(float dtime) {
 	L.pos.y += L.speed.y * dtime;
 }
 
+static void check_collision_grounded() {
+	float a, b;
 
-static void check_collision() {
+	a = tilemap_wall_left(L.pos.x, L.pos.y+4);
+	
+	if(L.pos.x < a+7) {
+		// collision?
+		L.pos.x = a + 7;
+	}
+	
+	
+	a = tilemap_wall_right(L.pos.x, L.pos.y+4);
+	
+	if(L.pos.x > a-7) {
+		// collision?
+		L.pos.x = a - 7;
+	}
+	
+	
+	a = tilemap_ground(L.pos.x-4, L.pos.y);
+	b = tilemap_ground(L.pos.x+4, L.pos.y);
+	
+	if(L.pos.y <= a+20 || L.pos.y <= b+20) {
+		L.pos.y = sca_max(a, b) + 14;
+	} else {
+		L.state = HARE_FALLING;
+		L.speed.y = 0;
+	}
+}
+
+
+static void check_collision_falling() {
 	float a, b;
 	
 	a = tilemap_ceiling(L.pos.x-4, L.pos.y+4);
@@ -92,29 +127,22 @@ static void check_collision() {
 		L.pos.x = sca_min(a, b) - 7;
 	}
 	
-	
-	if(L.jump_time>0.2 && L.jump_time<0.8)
-	    return;
-	
 	a = tilemap_ground(L.pos.x-4, L.pos.y-4);
 	b = tilemap_ground(L.pos.x+4, L.pos.y-4);
 	L.coll[6].rect.pose=u_pose_new(L.pos.x-4, a, 3, 3);
 	L.coll[7].rect.pose=u_pose_new(L.pos.x+4, b, 3, 3);
 	
-	if(L.pos.y <= a+16 || L.pos.y <= b+16) {
-		L.grounded = true;
+	if(L.pos.y <= a+13 || L.pos.y <= b+13) {
 		L.pos.y = sca_max(a, b) + 14;
+		L.state = HARE_GROUNDED;
 		L.speed.y = 0;
-		if(L.jump_time >0.8)
-		    L.jump_time = -1;
-	} else {
-		L.grounded = false;
 	}
 	
 }
 
 
 void hare_init() {
+	L.state = HARE_GROUNDED;
 	L.jump_time = -1;
 	
 	r_ro_single_init(&L.ro, camera.gl_main, r_texture_init_file("res/hare.png", NULL));
@@ -130,35 +158,49 @@ void hare_init() {
 
 void hare_update(float dtime) {
     char text[64];
-    sprintf(text, "%+07.3f %i", L.set_speed_x, L.set_jump);
+    sprintf(text, "%i %+08.3f %+05.3f\n ", L.state, L.set_speed_x, L.set_jump_time);
     
-    
-    if(L.grounded && L.set_jump && L.jump_time <0) {
-    	L.jump_time = 0;
+    if(L.set_jump_time <= 0) {
+    	if(L.state == HARE_GROUNDED) {
+    	    L.state = HARE_JUMPING;
+    	    L.jump_time = 0;
+    	    L.set_jump_time = 1;
+        }
+        L.set_jump_time+=dtime;
     }
-    L.set_jump = false;
     
-    if(L.jump_time>=0)
-        L.jump_time+=dtime;
+    L.jump_time += dtime;
+    
+    if(L.state == HARE_JUMPING) {
+        if(L.jump_time>=0.1) {
+        	L.speed.y = JUMP_SPEED;
+        	L.state = HARE_FALLING;
+        }
+    }
         
-    if(L.jump_time>=0.1 && L.grounded) {
-    	L.grounded = false;
-    	L.speed.y = MAX_SPEED_Y;
-    }
-    
     apply_speed_x(dtime);
-    if(!L.grounded)
+    if(L.state != HARE_GROUNDED)
         apply_speed_y(dtime);
     
-    sprintf(text + strlen(text), " %+07.3f %+08.3f", 
+    sprintf(text + strlen(text), " %+08.3f %+08.3f", 
                 L.speed.x, L.speed.y);
-    
-	check_collision();
+    switch(L.state) {
+    	case HARE_GROUNDED:
+    	case HARE_JUMPING:
+    	    check_collision_grounded();
+    	    break;
+    	case HARE_FALLING:
+    	case HARE_DOUBLE_JUMP:
+    	    check_collision_falling();
+    	    break;
+    	default:
+    	    assert(0 && "invalid hare state");
+    }
 	
 	
 	
 	int frame;
-	if(L.grounded && L.jump_time<0) {
+	if(L.state == HARE_GROUNDED) {
 		float fps = 6;
 		int frames = 4;
 		static float time = 0;
@@ -185,7 +227,7 @@ void hare_update(float dtime) {
 		L.looking_left = false;
 
 	float v;
-	if(L.grounded && L.jump_time<0) {
+	if(L.state == HARE_GROUNDED) {
 	    v = sca_abs(L.speed.x) < MIN_SPEED_X? 0 : 1;
 	    if (sca_abs(L.speed.x) >=MAX_SPEED_X)
 		    v++;
@@ -212,7 +254,11 @@ void hare_render() {
 	r_ro_single_render(&L.ro);
 	//for(int i=0; i<8;i++)
 	//    r_ro_single_render(&L.coll[i]);
-	//r_ro_text_render(&L.input_text);
+	r_ro_text_render(&L.input_text);
+}
+
+enum hare_state hare_state() {
+	return L.state;
 }
 
 vec2 hare_position() {
@@ -225,5 +271,5 @@ void hare_set_speed(float dx) {
 }
 
 void hare_jump() {
-	L.set_jump = true;
+	L.set_jump_time = SET_JUMP_TIME;
 }
