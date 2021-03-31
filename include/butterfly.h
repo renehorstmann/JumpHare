84c10a85b204c14e212e227a7aba99830b484f52
renehorstmann <r.horsty@gmail.com>
2021-03-31 10:51:09 +0200

    gpush

---
+++ include/butterfly.h
@@ -0,0 +1,15 @@
+#ifndef JUMPHARE_BUTTERFLY_H
+#define JUMPHARE_BUTTERFLY_H
+
+#include <stdbool.h>
+#include "mathc/types/float.h"
+
+void butterfly_init(const vec2 *positions, int num);
+
+void butterfly_kill();
+
+void butterfly_update(float dtime);
+
+void butterfly_render();
+
+#endif //JUMPHARE_BUTTERFLY_H
---
+++ src/butterfly.c
@@ -0,0 +1,34 @@
+#include "r/ro_particle.h"
+#include "r/texture.h"
+#include "u/pose.h"
+#include "mathc/float.h"
+#include "mathc/utils/random.h"
+#include "utilc/assume.h"
+#include "camera.h"
+#include "hare.h"
+#include "butterfly.h"
+
+#define FPS 6.0
+#define FRAMES 12
+
+
+static struct {
+    rRoParticle ro;
+    float time;
+} L;
+
+void butterfly_init(const vec2 *positions, int num) {
+    
+}
+
+void butterfly_kill() {
+    
+}
+
+void butterfly_update(float dtime) {
+    
+}
+
+void butterfly_render() {
+    
+}
