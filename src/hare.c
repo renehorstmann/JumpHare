5694ed2b9008d19c8dfd0ee1d4e8621fa012f576
renehorstmann <r.horsty@gmail.com>
2021-04-26 15:01:10 +0200

    gpush

--- src/hare.c
+++ src/hare.c
@@ -402,8 +402,8 @@ static void check_state_change(float dtime) {
 
 static void on_pause_callback(bool resume, void *ud) {
     log_info("hare: pause callback");
-    if(resume && L.state == HARE_GROUNDED)
-        hare_set_sleep(true);
+    if(resume &&  L.state == HARE_GROUNDED)
+        hare_set_sleep(false);
 }
 
 
