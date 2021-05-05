7e02defda0f17811c030eb4657a94630e8f43a25
renehorstmann <r.horsty@gmail.com>
2021-05-05 16:41:28 +0200

    gpush

--- src/enemies.c
+++ src/enemies.c
@@ -3,6 +3,8 @@
 #include "mathc/float.h"
 #include "rhc/error.h"
 #include "rhc/log.h"
+#include "hare.h"
+#include "collision.h"
 #include "camera.h"
 #include "enemies.h"
 
@@ -56,14 +58,63 @@ void enemies_render() {
 
 
 
+static void hedgehog_collision_cb(vec2 delta, enum collision_state state, void *ud) {
+    rRect_s *rect = ud;
+    bool left = u_pose_get_w(rect->uv) > 0;
+    vec2 pos = u_pose_get_xy(rect->pose);
+    
+    if(state != COLLISION_BOTTOM) {
+        pos.x += left? 3 : -3;
+        rect->uv = u_pose_new(0, 0, 
+                left? -1 : 1,
+                1);
+        return;
+    }
+    
+    pos = vec2_add_vec(pos, delta);
+    
+    u_pose_set_xy(&rect->pose, pos.x, pos.y);
+} 
+
 static void hedgehog_update(Type *self, float dtime) {
     const int frames = 4;
     const float fps = 6;
     self->time = sca_mod(self->time + dtime, frames / fps);
     int frame = self->time * fps;
+    
     for(int i=0; i<self->ro.num; i++) {
-        self->ro.rects[i].sprite.x = frame;
+        vec2 pos = u_pose_get_xy(self->ro.rects[i].pose);
+        float distance = vec2_distance(
+                hare_position(), 
+                pos);
+        if(distance < 45) {
+            int roll_frame = 0;
+            if(distance < 30)
+                roll_frame = 3;
+            else if(distance < 35)
+                roll_frame = 2;
+            else if(distance < 40)
+                roll_frame = 1;
+            self->ro.rects[i].sprite = (vec2) {{roll_frame, 1}};
+        } else {
+            self->ro.rects[i].sprite = (vec2) {{frame, 0}};
+            
+            bool left = u_pose_get_w(self->ro.rects[i].uv) > 0;
+            
+            float speed_x = left? -10 : 10;
+            pos.x += speed_x * dtime;
+            
+            u_pose_set_xy(&self->ro.rects[i].pose, pos.x, pos.y);
+            
+            Collision_s coll = {hedgehog_collision_cb, &self->ro.rects[i]};
+            vec2 center = {{pos.x, pos.y-6}};
+            vec2 radius = {{8, 8}};
+            vec2 speed = {.x = speed_x};
+            
+            collision_tilemap_grounded(coll, center, radius, speed);
+        }
     }
+    
     ro_batch_update(&self->ro);
 }
 
